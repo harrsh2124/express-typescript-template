837fd87fc9f97837612267f37a3581ec5e094090
Harrsh Patel <me.harrsh.patel@gmail.com>
2022-09-13 18:01:35 +0530

    Added Prisma

--- src/app/http/controllers/PingController.ts
+++ src/app/http/controllers/PingController.ts
@@ -1,13 +1,24 @@
 import { Request, Response } from 'express';
+import { prisma } from '../../providers/db';
 
 import { logger } from '../../providers/logger';
 
 export class PingController {
   public static async pong(req: Request, res: Response): Promise<Response> {
     logger.debug('Server Pinged');
+
+    await prisma.user.create({
+      data: {
+        email: `john_${Math.random()}@yopmail.com`,
+        name: 'John Doe',
+        age: 21,
+      },
+    });
+    const user = await prisma.user.findMany();
     return res.status(200).json({
       status: true,
       message: 'pong',
+      user,
     });
   }
 }
